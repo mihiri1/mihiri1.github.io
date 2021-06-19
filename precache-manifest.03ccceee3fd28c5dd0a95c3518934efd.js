c3be5e975a376eea60453c721d2054505c57276c
mihiri1 <narmadamihiri1@gmail.com>
2021-06-20 02:15:55 +0530

    Updates

---
+++ precache-manifest.03ccceee3fd28c5dd0a95c3518934efd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2168dd66b6d20e64adcb80da07cf1ef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7cccbf65d8daa302e05",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cccbf65d8daa302e05",
+    "url": "/static/js/main.5cbc30f8.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "967750b9b7229f6a287e5fabbc2adb6c",
+    "url": "/static/media/IMG_0277.967750b9.jpg"
+  },
+  {
+    "revision": "ed968cbb4960980d31aa350a1e67a444",
+    "url": "/static/media/IMG_7480.ed968cbb.JPG"
+  },
+  {
+    "revision": "10f45ae5b84506b072dd79409e44e7ae",
+    "url": "/static/media/resume.10f45ae5.pdf"
+  }
+]);
